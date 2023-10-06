68a40053d39485f34ae86bbca5ed65e1166b84fd
papersgod <44570873+papersgod@users.noreply.github.com>
2023-10-06 23:45:58 +0700

    Add project files.

---
+++ MVVMEssentials/Commands/CreateCommand.cs
@@ -0,0 +1,7 @@
+﻿using MVVMEssentials.ViewModels;
+using System.Windows.Input;
+
+namespace MVVMEssentials.Commands
+{
+    public delegate ICommand CreateCommand<TViewModel>(TViewModel viewModel) where TViewModel : ViewModelBase;
+}
---
+++ MVVMEssentials/ViewModels/ErrorsViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace MVVMEssentials.ViewModels
+{
+    public class ErrorsViewModel : ViewModelBase, INotifyDataErrorInfo
+    {
+        private readonly Dictionary<string, List<string>> _propertyErrors = new Dictionary<string, List<string>>();
+
+        public bool HasErrors => _propertyErrors.Any();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public Dictionary<string, List<string>> PropertyErrors
+        {
+            get => _propertyErrors;
+        }
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return PropertyErrors.TryGetValue(propertyName, out var propertyErrors) ? propertyErrors : null;
+        }
+
+
+        public void AddError(string propertyName, string errorMessage)
+        {
+            if (!_propertyErrors.ContainsKey(propertyName))
+            {
+                _propertyErrors.Add(propertyName, new List<string>());
+            }
+
+            _propertyErrors[propertyName].Add(errorMessage);
+            OnErrorsChanged(propertyName);
+        }
+
+        public void ClearErrors(string propertyName)
+        {
+            if (_propertyErrors.Remove(propertyName))
+            {
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        private void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+    }
+}
